dda96d94e588a039d0485612c4bd0487fd3a27e9
VKozlov <voloda1992@gmail.com>
2019-06-10 13:34:23 +0300

    stop loss fix

--- ta4j-core/src/main/java/org/ta4j/core/trading/rules/CustomStopLossRule.java
+++ ta4j-core/src/main/java/org/ta4j/core/trading/rules/CustomStopLossRule.java
@@ -4,7 +4,6 @@ import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.ta4j.core.Bar;
 import org.ta4j.core.TimeSeries;
-import org.ta4j.core.Trade;
 import org.ta4j.core.TradingRecord;
 import org.ta4j.core.indicators.helpers.ClosePriceIndicator;
 import org.ta4j.core.num.Num;
@@ -48,29 +47,31 @@ public class CustomStopLossRule extends org.ta4j.core.trading.rules.StopLossRule
         }
         boolean satisfied = super.isSatisfied(index, tradingRecord);
 
-        Num entryPrice = tradingRecord.getCurrentTrade().getEntry().getPrice();
-        Num currentPrice = closePrice.getValue(index);
-        Num threshold = entryPrice.multipliedBy(lossRatioThreshold);
-        Bar bar = series.getBar(index);
-        if (satisfied) {
-            setEventType(tradingRecord, String.format("satisfied by stop loss " +
-                            "start time: %s " +
-                            "end time: %s " +
-                            "current price: %s " +
-                            "threshold: %s",
+        if (tradingRecord.getCurrentTrade().isOpened()) {
+            Num entryPrice = tradingRecord.getCurrentTrade().getEntry().getPrice();
+            Num currentPrice = closePrice.getValue(index);
+            Num threshold = entryPrice.multipliedBy(lossRatioThreshold);
+            Bar bar = series.getBar(index);
+            if (satisfied) {
+                setEventType(tradingRecord, String.format("satisfied by stop loss " +
+                                "start time: %s " +
+                                "end time: %s " +
+                                "current price: %s " +
+                                "threshold: %s",
+                        bar.getBeginTime(),
+                        bar.getEndTime(),
+                        currentPrice,
+                        threshold));
+            }
+            log.info("StopLossRule satisfied: {} start time: {},  end time: {}, current price: {}, threshold: {}, index {} ",
+                    satisfied,
                     bar.getBeginTime(),
                     bar.getEndTime(),
                     currentPrice,
-                    threshold));
+                    threshold,
+                    index);
         }
 
-        log.info("StopLossRule satisfied: {} start time: {},  end time: {}, current price: {}, threshold: {}, index {} ",
-                satisfied,
-                bar.getBeginTime(),
-                bar.getEndTime(),
-                currentPrice,
-                threshold,
-                index);
         return satisfied;
     }
 }
